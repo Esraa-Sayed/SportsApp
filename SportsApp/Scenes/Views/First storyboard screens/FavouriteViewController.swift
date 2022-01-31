dc4723774cc884f72511f43f7aec3e0e3279faea
Esraa-Sayed <esraasayedahmed1999@gmail.com>
2022-01-31 11:53:20 +0200

    add FavouriteViewController

---
+++ SportsApp/Scenes/Views/First storyboard screens/FavouriteViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FavouriteViewController.swift
+//  SportsApp
+//
+//  Created by esraa on 1/31/22.
+//  Copyright © 2022 esraa. All rights reserved.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
