c387871f5b03120e120a3427212eb2714c7fd02f
Esraa-Sayed <esraasayedahmed1999@gmail.com>
2022-02-01 13:19:39 +0200

    Add Toast Class

--- SportsApp/Scenes/Views/First storyboard screens/First Screen/CollectionFirstScreen.swift
+++ SportsApp/Scenes/Views/First storyboard screens/First Screen/CollectionFirstScreen.swift
@@ -23,7 +23,7 @@ class CollectionFirstScreen: UICollectionViewController {
         presenter = AllSportsVCPresenter(view: self)
         if !presenter.viewDidLoad()
         {
-            showToast(controller: self, message : "No internet connection", seconds: 4.0)
+            Toast.showToast(controller: self, message : "No internet connection", seconds: 4.0)
         }
     }
 
@@ -46,20 +46,6 @@ class CollectionFirstScreen: UICollectionViewController {
         presenter.configure(cell: cell, forIndex: indexPath.row)
     
         return cell
-    }
-    func showToast(controller: UIViewController, message : String, seconds: Double) {
-        let alert = UIAlertController(title: nil, message:nil, preferredStyle: .alert)
-        //rgb(0, 129, 138)
-        alert.view.backgroundColor =  UIColor(red: 100.0/255.0, green: 130.0/255.0, blue: 230.0/255.0, alpha: 0.8)
-       
-        let titleAttributes = [NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-Bold", size: 20)!, NSAttributedString.Key.foregroundColor:UIColor(red: 0.0/255.0, green: 129.0/255.0, blue: 138.0/255.0, alpha: 1.0)]
-        let titleString = NSAttributedString(string: message, attributes: titleAttributes)
-        alert.setValue(titleString, forKey: "attributedTitle")
-        alert.view.layer.cornerRadius = 15
-        controller.present(alert, animated: true)
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
-            alert.dismiss(animated: true)
-        }
     }
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         presenter.didSelectedRow(index: indexPath.row)
